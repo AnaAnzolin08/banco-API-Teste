236d674bbab07aa9b8e1797b4c98c02f53c7928d
AnaAnzolin08 <anaa.anzolin@hotmail.com>
2025-07-09 11:14:30 -0300

    Adicionando fixtures para  gerir os corpos de requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
   describe('POST /transferencias', () => {
@@ -11,35 +12,31 @@ describe('Transferências', () => {
          token = await obterToken('Julio.lima', '123456');
 
     })
-
+    
     it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de 10,00', async () => {
-        
+    const bodyTraferencias = { ...postTransferencias }
+
       const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
-        .set('Content-Type', 'application/json') // Removido espaço extra!
+        .set('Content-Type', 'application/json') 
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 11
-        });
+        .send(bodyTraferencias)
 
       expect(resposta.status).to.equal(201);
     });
 
     it('Deve retornar falha 422 quando o valor da transferência for abaixo de 10,00', async () => {
-
-      const resposta = await request(process.env.BASE_URL)
+        const bodyTraferencias = {...postTransferencias}
+        bodyTraferencias.valor = 8
+    
+        const resposta = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`)
-        .send({
-          contaOrigem: 1,
-          contaDestino: 2,
-          valor: 8
-        });
+        .send(bodyTraferencias)
 
-      expect(resposta.status).to.equal(422); // Corrigido: 422 é unprocessable entity
-    });
-  });
-});
+        expect(resposta.status).to.equal(422)
+        })
+    })
+
+})
